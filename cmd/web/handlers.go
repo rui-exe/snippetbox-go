8c8950f6ab363ccda6e5af9705b8ee1975425d56
rui-exe <mrswager173@gmail.com>
2024-11-19 11:32:48 +0000

    Pag 182

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,10 +10,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -24,8 +20,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		Snippets: s,
 	})
 }
+
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// Pat doesn't strip the colon from the named capture key, so we need to
+	// get the value of ":id" from the query string instead of "id".
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -39,30 +38,26 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// Use the new render helper.
 	app.render(w, r, "show.page.tmpl", &templateData{
 		Snippet: s,
 	})
 }
 
+// Add a new createSnippetForm handler, which for now returns a placeholder response.
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-	// Create some variables holding dummy data. We'll remove these later on
-	// during the build.
+	// Checking if the request method is a POST is now superfluous and can be
+	// removed.
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := "7"
-	// Pass the data to the SnippetModel.Insert() method, receiving the
-	// ID of the new record back.
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	// Redirect the user to the relevant page for the snippet.
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	// Change the redirect to use the new semantic URL style of /snippet/:id
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,21 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat" // New import
 	"github.com/justinas/alice"
 )
 
-// Update the signature for the routes() method so that it returns a
-// http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
-	// Create a middleware chain containing our 'standard' middleware
-	// which will be used for every request our application receives.
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet)) // Moved down
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// Wrap the existing chain with the recoverPanic middleware.
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 	return standardMiddleware.Then(mux)
 }
